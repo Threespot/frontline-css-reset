f9d51c242972c2c3ac21c2f59843c1ffa7ee4603
Ted Whitehead <ted.whitehead@threespot.com>
2015-11-09 15:39:10 -0500

    Initial commit

---
+++ Gulpfile.js
@@ -0,0 +1,84 @@
+'use strict';
+// -----------------------------------------------------------------------------
+// Sass
+// -----------------------------------------------------------------------------
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var pixrem = require('gulp-pixrem');
+var rename = require('gulp-rename');
+var minifyCSS = require('gulp-minify-css');
+var notify = require('gulp-notify');
+
+//------------------------------------------------------------------------
+// Error handling
+//------------------------------------------------------------------------
+var handleErrors = function(errorObject, callback) {
+  notify.onError(errorObject.toString().split(': ').join(':\n')).apply(this, arguments);
+  // Keep gulp from hanging on this task
+  if (typeof this.emit === 'function') {
+    this.emit('end');
+  }
+};
+
+//------------------------------------------------------------------------
+// Build
+//------------------------------------------------------------------------
+gulp.task('sass', function() {
+  return gulp.src('*.scss')
+  .pipe( sass({
+      outputStyle: 'expanded',
+      precision: 6
+    }))
+    .on('error', handleErrors)
+    // Add rem fallbacks using pixrem
+    .pipe( pixrem() )// options https://github.com/robwierzbowski/node-pixrem#options
+    .on('error', handleErrors)
+    .pipe( autoprefixer({
+      browsers: [
+        'ie >= 8',
+        'ie_mob >= 10',
+        'ff >= 25',
+        'chrome >= 32',
+        'safari >= 7',
+        'opera >= 19',
+        'ios >= 6',
+        'android >= 2',
+        'bb >= 10'
+      ],
+      cascade: false
+    }))
+    .on('error', handleErrors)
+    .pipe( gulp.dest('./dist'))
+    // Minify
+    .pipe( minifyCSS({
+      advanced: false,
+      // TODO: urlQuotes does not seem to be working
+      compatibility: {
+        '*': {
+          properties: {
+            urlQuotes: true// Required for inline SVGs
+          }
+        }
+      }
+    }))
+    .pipe( rename({ suffix: '.min' }) )
+    .pipe( gulp.dest('./dist'))
+});
+
+// -----------------------------------------------------------------------------
+// Lint
+// -----------------------------------------------------------------------------
+var sassLint = require('gulp-sass-lint');
+
+gulp.task('lint', function() {
+  return gulp.src('*.scss')
+    .pipe(sassLint())
+    .pipe(sassLint.format())
+    .pipe(sassLint.failOnError());
+});
+
+// -----------------------------------------------------------------------------
+// Default task
+// -----------------------------------------------------------------------------
+gulp.task('default', ['sass', 'lint']);
--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# frontline-css-reset
+# Frontline CSS Reset
 Threespot's CSS reset
+
+## License
+
+Frontline CSS Reset is free software, and may be redistributed under the terms of the [MIT license](https://github.com/Threespot/frontline-css-reset/blob/master/LICENSE.md). If Frontline CSS Reset works great for your project, [we'd love to hear about it](http://twitter.com/threespot)!
+
+## Thanks
+
+Our work stands on the shoulders of giants. We're very thankful to the many people that made Frontline CSS Reset possible, either by publishing code we used or by being an inspiration for this project.
+
+- [http://meyerweb.com/eric/tools/css/reset/](http://meyerweb.com/eric/tools/css/reset/)
+- [https://github.com/necolas/normalize.css](https://github.com/necolas/normalize.css)
+- [https://github.com/10up/sanitize.css](https://github.com/10up/sanitize.css)
+
+## About Threespot
+
+Threespot is a design and development agency from Washington, DC. We work for organizations that we believe are making a positive change in the world. Find out more [about us](https://www.threespot.com), [our projects](https://www.threespot.com/work) or [hire us](https://www.threespot.com/agency/hire-us)!
+
+[![Threespot](https://avatars3.githubusercontent.com/u/370822?v=3&s=100)](https://www.threespot.com)
